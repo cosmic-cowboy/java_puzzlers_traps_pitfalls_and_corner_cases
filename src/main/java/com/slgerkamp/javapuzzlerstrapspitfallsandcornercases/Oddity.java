11dea9e6d95d3475827f06088bff64da2b077a77
hiroo takaku <slgerkamp@gmail.com>
2014-06-28 16:12:17 +0900

    Puzzle 1: Oddity：解答

--- src/main/java/com/slgerkamp/javapuzzlerstrapspitfallsandcornercases/Oddity.java
+++ src/main/java/com/slgerkamp/javapuzzlerstrapspitfallsandcornercases/Oddity.java
@@ -9,6 +9,10 @@ package com.slgerkamp.javapuzzlerstrapspitfallsandcornercases;
 public class Oddity {
     public static boolean isOdd( int i ){
     	// 値を絶対値にする
-    	return Math.abs(i) % 2 == 1;
+//    	return Math.abs(i) % 2 == 1;
+    	// 模範回答
+//    	return i % 2 != 0;
+    	// 模範回答 ビット積演算子を利用
+    	return (i & 1) != 0;
     }
 }
